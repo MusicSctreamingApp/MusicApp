46f21b1e094d492d920c9b6bcceb6495135882ca
lingtaoling <oktaoling@gmail.com>
2022-09-18 23:29:13 -0400

    test

--- musicapp-frontend/src/components/Navbar.js
+++ musicapp-frontend/src/components/Navbar.js
@@ -5,6 +5,7 @@ import { useAuthContext } from "../hooks/useAuthContext";
 const Navbar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
+  console.log("test");
 
   const handleClick = () => {
     logout();
