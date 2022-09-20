96fb41bc6b9f3a9f6c49dfa75089f51503abf424
Louis <louis.dumasv@gmail.com>
2022-09-20 16:43:48 -0400

    test

--- musicapp-frontend/src/pages/AdminPanel.js
+++ musicapp-frontend/src/pages/AdminPanel.js
@@ -1,8 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { useEffect, useState } from "react";
 
 const AdminPanel = () => {
+  const [state, setState] = useState([]);
   const handleSideBarButtons = () => {};
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      const json = await response.json();
+
+      if (response.ok) {
+        setState(json);
+      }
+    };
+    if (state) {
+      fetchUsers();
+    }
+  }, []);
 
   return (
     <div className="container-fluid">
@@ -34,52 +53,22 @@ const AdminPanel = () => {
 
         <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 className="h2">Dashboard</h1>
-            <div className="btn-toolbar mb-2 mb-md-0">
-              <div className="btn-group me-2">
-                <button
-                  type="button"
-                  className="btn btn-sm btn-outline-secondary"
-                >
-                  Share
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-sm btn-outline-secondary"
-                >
-                  Export
-                </button>
-              </div>
-              <button
-                type="button"
-                className="btn btn-sm btn-outline-secondary dropdown-toggle"
-              >
-                <span data-feather="calendar"></span>
-                This week
-              </button>
-            </div>
+            <h1 className="h2">Admin Panel</h1>
           </div>
-
-          <canvas
-            className="my-4 w-100"
-            id="myChart"
-            width="900"
-            height="380"
-          ></canvas>
-
-          <h2>Section title</h2>
+          <h2>Users</h2>
           <div className="table-responsive">
             <table className="table table-striped table-sm">
               <thead>
                 <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
+                  <th scope="col">#id</th>
+                  <th scope="col">email</th>
+                  <th scope="col">role</th>
+                  <th scope="col">Edit</th>
+                  <th scope="col">Ban</th>
                 </tr>
               </thead>
               <tbody>
+              {user && user.map((user) => )}
                 <tr>
                   <td>1,001</td>
                   <td>random</td>
