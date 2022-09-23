80ccb8bf6c878460aa9faafdedf641a0993d0885
Ling Tao <oktaoling@gmail.com>
2022-09-23 00:39:31 -0400

    feat: add AddSongForm back

---
+++ musicapp-frontend/src/components/MyAlbum.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react';
+import { useAuthContext } from "../hooks/useAuthContext";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+// not finish yet 
+function MyAlbum() {
+  let { id } = useParams();
+
+  return (
+    <div className="App app ">
+
+
+      <Link className="button" to={"/addsong/" + id}
+      > Add new Song</Link>
+
+
+    </div>
+  );
+}
+
+export default MyAlbum;
