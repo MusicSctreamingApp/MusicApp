7a391449519a92684c6ffcae52705bf885b1d3d1
Xuan Wang <xuan.wang3@mail.mcgill.ca>
2022-09-24 23:50:05 -0400

    update the home page for demo

--- musicapp-frontend/src/pages/Home.js
+++ musicapp-frontend/src/pages/Home.js
@@ -1,74 +1,120 @@
-import { useEffect } from "react";
-import axios from "axios";
-import { useNavigate, Link } from "react-router-dom";
-import { useSongsContext } from "../hooks/useSongsContext";
-import { useAuthContext } from "../hooks/useAuthContext";
-import AddSongFormContext from "../components/AddSongForm";
-//components
-import SongsGrid from "../components/SongsGrid";
-const Home = () => {
-  const { songs, dispatch } = useSongsContext();
-  const { user } = useAuthContext();
-  const user_kk = JSON.parse(localStorage.getItem('user'))
-  let history = useNavigate();
-  useEffect(() => {
-    const fetchSongs = async () => {
-      const response = await fetch("/api/songs", {
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
-      });
-      const json = await response.json();
-      if (response.ok) {
-        dispatch({ type: "SET_SONGS", payload: json });
-      }
-    };
-    if (user) {
-      fetchSongs();
+import { useState } from 'react'
+import imgs from './assets/1.jpg'
+import  '../styles/home.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const list = [
+    {
+      value: "fdffdsf",
+      label: "fdsffdsffdsffdsffdsffdsffdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
     }
-    if(null === user.token){
-      const fetchSongs = async () => {
-        // Everyone can browsing the music
-        const response = await fetch("/api/songs");
-        const json = await response.json();
-        if (response.ok) {
-          dispatch({ type: "SET_SONGS", payload: json });
-        }
-      }
-    }else{
-      const fetchSongs = async () => {
-        const response = await fetch("/api/songs", {
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        });
-        const json = await response.json();
-        if (response.ok) {
-          dispatch({ type: "SET_SONGS", payload: json });
-        }
-      };
-      
-      // Login user
-      if (user_kk) {
-        // Regular user
-        if (user_kk.user.role === "USER") {
-          fetchSongs();
-        }else{
-          // Admin
-          history("/");
-        }
-      }
-    }
-  }, [dispatch, user]);
+  ];
   return (
-    <div className="home">
-      <div className="songs"> 
-        {user_kk.user.role === 'ADMIN' ? <Link to="/"> Create A Post</Link> : ""}
-        <AddSongFormContext/>
-        {songs && songs.map((song) => <SongsGrid key={song._id} song={song} />)}
+    <div className="App">
+      <div className='head'>
+        <div className='box3'>
+          <div className='title'>Good afternoon</div>
+          <div className=' w10' > 
+          <div className='btn'>Admin</div>
+          </div>
+        </div>
       </div>
+      <div className='box mt-30'>
+          <div className='item'>
+            <img src={imgs} className="img1" />
+            <div className='bg1 '>
+              Album1
+            </div>
+          </div>
+          <div className='item'>
+            <img src={imgs} className="img1" />
+            <div className='bg1 '>
+              Album2
+            </div>
+          </div>
+         <div className='item'>
+            <img src={imgs} className="img1" />
+            <div className='bg1 '>
+              Album3
+            </div>
+          </div>
+      </div>
+      <div className='mt-50'>
+        <div className='box3'>
+          <div className='title'> Top</div>
+          <div className='title2 w5'>See All</div>
+        </div>
+        <div className='box'>
+          {list.map((item, idx) => (
+            <div className='item2 bg2 '>
+                <img src={imgs} className="img2" />
+                <div>
+                  <div className=' ft15 wb mt-10 '>
+                    {item.value}
+                  </div>
+                  <div className='txt mt-10 vtxt1'>
+                  {item.label}
+                  </div>
+                </div>
+            </div>
+            ))
+          }         
+        </div>
+      </div>
+      <div className='mt-50'>
+        <div className='box3'>
+          <div className='title'>Top </div>
+          <div className='title2 m36'>See All</div>
+        </div>
+        <div className='box'>
+          {list.map((item, idx) => (
+            <div className='item2  '>
+                <img src={imgs} className="img3" />
+            </div>
+            ))
+          }         
+        </div>
+      </div>
+      
     </div>
-  );
-};
-export default Home;
-//test 
+  )
+}
+
+export default App
---
+++ musicapp-frontend/src/pages/Home_bak.js
@@ -0,0 +1,74 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import { useSongsContext } from "../hooks/useSongsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
+import AddSongFormContext from "../components/AddSongForm";
+//components
+import SongsGrid from "../components/SongsGrid";
+const Home = () => {
+  const { songs, dispatch } = useSongsContext();
+  const { user } = useAuthContext();
+  const user_kk = JSON.parse(localStorage.getItem('user'))
+  let history = useNavigate();
+  useEffect(() => {
+    const fetchSongs = async () => {
+      const response = await fetch("/api/songs", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const json = await response.json();
+      if (response.ok) {
+        dispatch({ type: "SET_SONGS", payload: json });
+      }
+    };
+    if (user) {
+      fetchSongs();
+    }
+    if(null === user.token){
+      const fetchSongs = async () => {
+        // Everyone can browsing the music
+        const response = await fetch("/api/songs");
+        const json = await response.json();
+        if (response.ok) {
+          dispatch({ type: "SET_SONGS", payload: json });
+        }
+      }
+    }else{
+      const fetchSongs = async () => {
+        const response = await fetch("/api/songs", {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        const json = await response.json();
+        if (response.ok) {
+          dispatch({ type: "SET_SONGS", payload: json });
+        }
+      };
+      
+      // Login user
+      if (user_kk) {
+        // Regular user
+        if (user_kk.user.role === "USER") {
+          fetchSongs();
+        }else{
+          // Admin
+          history("/");
+        }
+      }
+    }
+  }, [dispatch, user]);
+  return (
+    <div className="home">
+      <div className="songs"> 
+        {user_kk.user.role === 'ADMIN' ? <Link to="/"> Create A Post</Link> : ""}
+        <AddSongFormContext/>
+        {songs && songs.map((song) => <SongsGrid key={song._id} song={song} />)}
+      </div>
+    </div>
+  );
+};
+export default Home;
+//test 
