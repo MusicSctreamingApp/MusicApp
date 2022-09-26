471e1fc985f66f81cd1346876bdf40d086efadfe
Louis <louis.dumasv@gmail.com>
2022-09-26 18:23:32 -0400

    before merge main with Xuan changes

--- musicapp-frontend/src/index.js
+++ musicapp-frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 import { SongsContextProvider } from "./context/SongsContext";
 import { AuthContextProvider } from "./context/AuthContext";
@@ -18,4 +18,3 @@ root.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-
