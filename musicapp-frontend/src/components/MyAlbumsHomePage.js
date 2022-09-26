37bc0033355531237a8735d2a12ace8554f309a8
github-xuanwang <35718382+github-xuanwang@users.noreply.github.com>
2022-09-26 02:06:45 -0400

    home page layout edit the navbar and myalbums

---
+++ musicapp-frontend/src/components/MyAlbumsHomePage.js
@@ -0,0 +1,91 @@
+
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { useAuthContext } from "../hooks/useAuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+// not finish yet
+function MyAlbums() {
+
+  let url = "https://spitifo.s3.amazonaws.com/";
+
+  const [albums, setAlbums] = useState([]);
+  const [album, setAlbum] = useState([]);
+  const { user } = useAuthContext();
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAlbums = async () => {
+      console.log(user);
+      const response = await fetch("/api/album/user", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+
+      const json = await response.json();
+
+      if (response.ok) {
+        setAlbums(json);
+
+      }
+    };
+    if (user) {
+      fetchAlbums();
+    }
+  }, [setAlbums, user]);
+
+  // const handleDelete = async (_id) => {
+  //   // need to to delete all songs by album_Id first.
+
+  //   const response = await fetch(`/api/album/${_id}`, {
+  //     method: "DELETE",
+  //     headers: {
+  //       Authorization: `Bearer ${user.token}`,
+  //     },
+  //   });
+
+  //   const json = await response.json();
+
+  //   if (response.ok) {
+  //     setAlbums(albums.filter((s) => s._id !== _id));
+  //   } else {
+  //     setError(json.error);
+  //   }
+  // };
+
+
+  return (
+
+    <div className="App app ">
+      {/* <div>
+        <Link className="button" to="/addalbum"> Add new Album</Link>
+
+      </div> */}
+
+
+      <div>
+
+        {albums &&
+          albums.map((album) => (
+            <div>
+
+              <Link className="button" to={"/myalbum/" + album._id}>
+                <img src={url + album.cover} alt="Pics" width="200" height="200" class="rounded float-left mt-0 ml-1 px-2 p-3" />
+              </Link>
+              {/* <button type="button" className="btn btn-warning" onClick={() => { handleDelete(album._id) }} >Delete</button> */}
+            </div>
+
+          ))}
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default MyAlbums;
---
+++ musicapp-frontend/src/pages/Home_bak2.js
@@ -0,0 +1,123 @@
+import { useState } from 'react'
+import imgs from './assets/1.jpg'
+import  '../styles/home.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function App() {
+  const [count, setCount] = useState(0)
+  const list = [
+    {
+      value: "fdffdsf",
+      label: "fdsffdsffdsffdsffdsffdsffdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    },
+    {
+      value: "fdsf",
+      label: "dfdsfdfdsfdfdsfdfdsfdfdsf"
+    }
+  ];
+  return (
+    <div className="App">
+      <div className='head'>
+        <div className='box3'>
+          <div className='title'>Good afternoon</div>
+          <div className=' w10' > 
+          <div className='btn'>Admin</div>
+          </div>
+        </div>
+      </div>
+      <div className='box mt-30'>
+          <div className='item'>
+            <img src={imgs} className="img1" />
+            <div className='bg1 '>
+              Album1
+            </div>
+          </div>
+          <div className='item'>
+            <img src={imgs} className="img1" />
+            <div className='bg1 '>
+              Album2
+            </div>
+          </div>
+         <div className='item'>
+            <img src={imgs} className="img1" />
+            <div className='bg1 '>
+              Album3
+            </div>
+          </div>
+      </div>
+      <div className='mt-50'>
+        <div className='box3'>
+          <div className='title'> Top</div>
+          <div className='title2 w5'>See All</div>
+        </div>
+        <div className='box'>
+          {list.map((item, idx) => (
+            <div className='item2 bg2 '>
+                <img src={imgs} className="img2" />
+                <div>
+                  <div className=' ft15 wb mt-10 '>
+                    {item.value}
+                  </div>
+                  <div className='txt mt-10 vtxt1'>
+                  {item.label}
+                  </div>
+                </div>
+            </div>
+            ))
+          }         
+        </div>
+      </div>
+      <div className='mt-50'>
+        <div className='box3'>
+          <div className='title'>Top </div>
+          <div className='title2 m36'>See All</div>
+        </div>
+        <div className='box'>
+          {list.map((item, idx) => (
+            <div className='item2  '>
+                <img src={imgs} className="img3" />
+            </div>
+            ))
+          }         
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
